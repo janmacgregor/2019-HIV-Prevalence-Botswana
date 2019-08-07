60af23bdcf6f33a1a0d3a05c2034e6a1507dac58
Herman Tolentino <hermantolentino@users.noreply.github.com>
2019-08-07 11:25:31 +0700

    Update aznbsetup.sh

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -1,10 +1,15 @@
 
 #!/bin/bash
-conda config --add channels conda-forge
-conda install -y \
-  matplotlib \
-  folium=0.9 \
-  osmnx=0.10 \
-  networkx \
-  pandas \
-  geopandas
+
+# Activate environment
+source /home/nbuser/anaconda3_420/bin/activate
+
+# Install packages
+conda update -c conda-forge conda conda-build
+conda install -y -c conda-forge folium=0.10* jinja2=2.10* pandas=0.25* geopandas=0.51*
+
+pip install --upgrade pip
+pip install 'opencv-python' 'opencv-contrib-python'
+
+
+source /home/nbuser/anaconda3_420/bin/deactivate
