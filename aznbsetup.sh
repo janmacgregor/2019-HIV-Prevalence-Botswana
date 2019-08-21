c6dd8c0321d41d2b9d2f00165015baf1ba05ecdb
Herman Tolentino <herman.tolentino@gmail.com>
2019-08-21 07:32:41 -0500

    updated scripts for Binder and Azure

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -6,7 +6,7 @@ source /home/nbuser/anaconda3_420/bin/activate
 
 # Install packages
 conda update -c conda-forge conda conda-build
-conda install -y -c conda-forge folium=0.10* jinja2=2.10* pandas=0.25* geopandas=0.51*
+conda install -y -c conda-forge folium=0.10* jinja2=2.10* pandas=0.25* geopandas=0.5* camelot-py
 
 pip install --upgrade pip
 pip install 'opencv-python' 'opencv-contrib-python'
