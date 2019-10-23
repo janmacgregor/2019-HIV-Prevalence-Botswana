3ad38c07d3499793dc8a99fbe62bddc8bd8ff2c5
James L. Tobias, MS, GISP <jtobias@cdc.gov>
2019-10-23 15:17:26 -0400

    Update aznbsetup.sh
    Jim Tobias making update to this Azure script to include conda install for descartes=1.1*

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -6,7 +6,7 @@ source /home/nbuser/anaconda3_420/bin/activate
 
 # Install packages
 conda update -c conda-forge conda conda-build
-conda install -y -c conda-forge folium=0.10* jinja2=2.10* pandas=0.25* geopandas=0.5* camelot-py
+conda install -y -c conda-forge folium=0.10* jinja2=2.10* pandas=0.25* geopandas=0.5* descartes=1.1* camelot-py
 
 pip install --upgrade pip
 pip install 'opencv-python' 'opencv-contrib-python'
