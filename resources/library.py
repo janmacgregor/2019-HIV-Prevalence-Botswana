9a6431c6c46547c310b89b8be2322dc9fa286bc4
James L. Tobias, MS, GISP <jtobias@cdc.gov>
2019-11-06 12:04:02 -0500

    Add files via upload
    Adding library.py and style.css to the resources folder so that we can increase font size of every notebook.

--- resources/library.py
+++ resources/library.py
@@ -1,20 +1,14 @@
-from IPython.display import HTML, IFrame
+from IPython.display import HTML
 import warnings
 
 def style_notebook():
     style = open("./resources/style.css", "r").read()
     return HTML(style)
 
-def show_video_file(url,width,height):
-    return HTML('<video width="900" height="600" controls autoplay><source src="'+url+'"</video>')
-
-def show_youtube(url, width, height):
-    return IFrame(url,width,height)
+def show_video(url, width=700, height=394):
+    return HTML('<iframe src="'+url+'?title=0&byline=0&portrait=0" width="'+str(width)+'" height="'+str(height)+'" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>')
 
 def play_mp3(file):
     return HTML('<audio src="'+file+'" controls><p>If you are reading this, it is because your browser does not support the audio element</p></audio>')
-
-def play_mp4(file, width, height):
-    return HTML("<video width='"+str(width)+"' height='"+str(height)+"' controls><source src='"+file+"' type='video/mp4'>Your browser does not support HTML5 video.</video>")
 
 warnings.filterwarnings('ignore')
